8076c9f80fde426ceaaed6f30e2345ac5993ea2e
HendrikClijstersMosa <hendrik.clijsters@mosa-rt.be>
2024-04-24 12:55:37 +0200

    Coupled login directly to loginBtn.

--- HergebruikendeCodeSite/navbar.js
+++ HergebruikendeCodeSite/navbar.js
@@ -1,10 +1,30 @@
-fetch("../HergebruikendeCodeSite/navbar.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("navigatiebalk").innerHTML = data;
-  });
+// Wait for the document to finish loading
+document.addEventListener('DOMContentLoaded', function () {
+  // Find all custom tags in the document
+  const customTags = document.querySelectorAll('navigatiebalk');
+
+  // Loop through each custom tag
+  customTags.forEach(function (tag) {
+    // Replace the custom tag with its inner HTML content
+    fetch("../HergebruikendeCodeSite/navbar.html")
+      .then(response => {
+        return response.text()
+      })
+      .then(data => {
+        tag.outerHTML = data
+      });
+
+      const netlifyIdentity =  document.createElement('script');
+      netlifyIdentity.src = "https://identity.netlify.com/v1/netlify-identity-widget.js"
+      document.body.appendChild(netlifyIdentity);
+
+      const netlifyCustom = document.createElement('script');
+      netlifyCustom.src = "/software/js/netlify.js";
+      document.body.appendChild(netlifyCustom);
+  }); 
+
+});
+
 
 window.onresize = function (e) {
   if (document.body.clientWidth > 1000) {
@@ -32,7 +52,7 @@ function onLogin(currentPage) {
     }, 100);
   } else if (netlifyIdentity.currentUser()) {
     document.getElementById('login').innerHTML = 'Data';
-    document.getElementById('login').setAttribute("onclick",  "location.href='/Pages/Data.html'");
+    document.getElementById('login').setAttribute("onclick", "location.href='/Pages/Data.html'");
     document.getElementById('login2').innerHTML = 'Data';
     document.getElementById('login2').href = '/pages/Data.html';
   }
@@ -47,12 +67,12 @@ function onLoginData(currentPage) {
     document.getElementById('login').innerHTML = 'Logout';
     document.getElementById('login2').innerHTML = 'Logout';
     document.getElementById('login').setAttribute("onclick", LogoutOnClick());
-    
+
 
   }
 }
 
-function LogoutOnClick(){
+function LogoutOnClick() {
   console.log("fucntie uitgzvoerd")
   netlifyIdentity.logout()
   window.href("index.html")
--- software/js/netlify.js
+++ software/js/netlify.js
@@ -1,49 +1,77 @@
-// Add event listeners for the following things.
-document.addEventListener('DOMContentLoaded', function () {
+// Load in all needed netlify buttons and link to functions containing onclick events.
+function addNetlifyEvents() {
     const loginBtn = document.getElementById('login');
-    const logoutBtn = document.getElementById('logout')
-    // const protectedPage = document.getElementById('protectedPage');
+    const logoutBtn = document.getElementById('logout');
+
+    if (loginBtn == null ) { // add after creating on logout: || logoutBtn == null
+        console.log("niet geladen")
+        setTimeout(() => {
+            addNetlifyEvents();
+        }, 100);
+    } else {
+        console.log("geladen")
+        loginBtn.onclick = onClickLogin;
+
+    }
+}
+
+// 
+function onClickLogin() {
+    if (netlifyIdentity) {
+        netlifyIdentity.open(); // Important! Change the config in Netlify to make it so signing up is disabled.
+    }
+}
+
+addNetlifyEvents()
+
+// // Add event listeners for the following things.
+// document.addEventListener('DOMContentLoaded', function () {
+//     console.log("Dit is een test???")
+
+//     const loginBtn = document.getElementById('login');
+//     const logoutBtn = document.getElementById('logout')
+//     // const protectedPage = document.getElementById('protectedPage');
 
     // if login btn is pressed, launch netlify login modal.
-    loginBtn.addEventListener('click', function () {
-        if (netlifyIdentity) {
-            netlifyIdentity.open(); // Important! Change the config in Netlify to make it so signing up is disabled.
-        }
-    });
-
-    // if logout btn is pressed, launch netlify logout function.
-    logoutBtn.addEventListener('click', function () {
-        if (netlifyIdentity) {
-            netlifyIdentity.logout();
-        }
-    });
-
-    // if protected page btn is pressed, only change page if user is logged in.
-    // protectedPage.addEventListener('click', function () {
-    //     const user = netlifyIdentity.currentUser();
-    //     if (user) {
-    //         window.location.href = '/Pages/Data.html';
+    // loginBtn.addEventListener('click', function () {
+    //     if (netlifyIdentity) {
+    //         netlifyIdentity.open(); // Important! Change the config in Netlify to make it so signing up is disabled.
     //     }
-
     // });
 
-    // if user logs in: print msg in log & change which button is visible.
-    netlifyIdentity.on('login', function (user) {
-        console.log('Welcome,', user);
-        window.location.href = '/Pages/Data.html';
+//     // if logout btn is pressed, launch netlify logout function.
+//     logoutBtn.addEventListener('click', function () {
+//         if (netlifyIdentity) {
+//             netlifyIdentity.logout();
+//         }
+//     });
 
-    });
+//     // if protected page btn is pressed, only change page if user is logged in.
+//     // protectedPage.addEventListener('click', function () {
+//     //     const user = netlifyIdentity.currentUser();
+//     //     if (user) {
+//     //         window.location.href = '/Pages/Data.html';
+//     //     }
 
-    // if user logs out: print msg in log & change which button is visible.
-    netlifyIdentity.on('logout', function () {
-        console.log('Logged out');
+//     // });
 
-    });
-});
+//     // if user logs in: print msg in log & change which button is visible.
+//     netlifyIdentity.on('login', function (user) {
+//         console.log('Welcome,', user);
+//         window.location.href = '/Pages/Data.html';
+
+//     });
+
+//     // if user logs out: print msg in log & change which button is visible.
+//     netlifyIdentity.on('logout', function () {
+//         console.log('Logged out');
+
+//     });
+// });
 
 
- // Upon page load in: check if user is logged. Print msg & change visible button depending on situation.
- function CheckLogin(){
+// Upon page load in: check if user is logged. Print msg & change visible button depending on situation.
+function CheckLogin() {
     window.href = "https://www.google.com"
     console.log('start functie');
     var button = document.getElementById('login');
@@ -51,7 +79,7 @@ document.addEventListener('DOMContentLoaded', function () {
     button2.innerHTML = "Data";
     button.innerHTML = "Data";
     button.href = "/pages/Data.html";
-    button2.href= "/pages/Data.html";
+    button2.href = "/pages/Data.html";
     console.log('heyfyuehfuzhfuhzuhf');
     // const user = netlifyIdentity.currentUser();
     // if (user) {
