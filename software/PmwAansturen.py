f51894700ee94e27259ae557691822c4ee7804b3
Thesmartbarn <thesmartbarn@outlook.com>
2024-05-17 14:25:01 +0200

    Automatic commit [skip netlify]

---
+++ software/PmwAansturen.py
@@ -0,0 +1,13 @@
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+pwm = GPIO.PWM(18, 1000)
+pwm.start(50)
+
+while True:
+    PwmWaarde = input("geef een pmw waarde in: ")
+    pwm.ChangeDutyCycle(PwmWaarde)
+
+    pwm.stop()
