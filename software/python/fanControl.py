d4573e121300c1632b0394f75fdadcb3328257ee
thesmartbarn <thesmartbarn@outlook.com>
2024-05-30 16:05:01 +0200

    Automatic commit [skip netlify]

--- software/python/fanControl.py
+++ software/python/fanControl.py
@@ -8,14 +8,18 @@ IO.setmode(IO.BCM)
 IO.setup(FAN_ON, IO.OUT)
 IO.setup(FAN_OFF, IO.OUT)
 
+IO.setmode(IO.BCM)
+IO.setup(18, IO.OUT)
+pwm = IO.PWM(18, 50)
+pwm.start(0)
 
-def setFanSpeed(calculatedSpeed):
-    if calculatedSpeed == 0: 
+def setFanSpeed(fanSpeed):
+    if fanSpeed == 0: 
         IO.output(FAN_OFF, IO.LOW)
         IO.output(FAN_ON, IO.HIGH)
     else:
         IO.output(FAN_ON, IO.LOW)
         IO.output(FAN_OFF, IO.HIGH)
-
+        pwm.ChangeDutyCycle(fanSpeed)
 
 
